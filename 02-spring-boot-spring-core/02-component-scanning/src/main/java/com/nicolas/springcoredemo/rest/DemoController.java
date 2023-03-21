d8e2c14acaef5c998ddb8f455ad2ebf8a9fdebca
Nicolas <nicolastessuto@gmail.com>
2023-03-21 14:49:52 -0300

    02-component-scanning, project organized

---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/nicolas/springcoredemo/rest/DemoController.java
@@ -0,0 +1,27 @@
+package com.nicolas.springcoredemo.rest;
+
+import com.nicolas.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //Define a private field for the dependency
+    private Coach coach;
+
+    //Define a constructor for dependency injection
+    @Autowired // Not necessary because we have just one constructor
+    public DemoController(Coach coach) {
+        this.coach = coach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkou(){
+        return coach.getDailyWorkout();
+    }
+
+
+
+}

0244cd5e3db9c0e30679618b4ffbedbb382c9f9d
Nicolas <nicolastessuto@gmail.com>
2023-05-19 20:02:48 -0300

    endpoint to return the list of employees created

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/nts/crudtraining/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/nts/crudtraining/rest/EmployeeRestController.java
@@ -1,4 +1,28 @@
 package com.nts.crudtraining.rest;
 
+import com.nts.crudtraining.dao.EmployeeDAO;
+import com.nts.crudtraining.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class EmployeeRestController {
+
+    private EmployeeDAO employeeDAO;
+
+    //Constructor injection (dirty method, that will be refactored later)
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll(){
+        return employeeDAO.findAll();
+    }
+
 }

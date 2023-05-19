104666aef3eabcc1e5029acfa23de0398701713f
nicolastessuto <nicolastessuto@gmail.com>
2023-05-19 14:17:48 -0300

    new project rest-crud with hibernate

---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/nts/crudtraining/CrudtrainingApplication.java
@@ -0,0 +1,13 @@
+package com.nts.crudtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudtrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudtrainingApplication.class, args);
+	}
+
+}
---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/test/java/com/nts/crudtraining/CrudtrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nts.crudtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudtrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
